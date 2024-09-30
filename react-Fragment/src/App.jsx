19e2e9a183bd2663de85e7fb03f25d63aa94cdd1
Haseebjaved607 <hasiimughal41@gmail.com>
2024-09-30 05:33:32 +0500

    fragments

---
+++ react-Fragment/src/App.jsx
@@ -0,0 +1,8 @@
+import FoodList from "./list";
+import "./app.css";
+
+function App() {
+  return <FoodList />;
+}
+
+export default App;
---
+++ react-Fragment/src/list.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function FoodList() {
+  return (
+    <>
+      <center className="center">
+        <h1 className="rf">Food list</h1>
+        <ul className="list-group rf">
+          <li className="list-group-item">Baryani</li>
+          <li className="list-group-item">Nehairi</li>
+          <li className="list-group-item">White karahi</li>
+          <li className="list-group-item">Beef pullao</li>
+          <li className="list-group-item">chicken tikka</li>
+        </ul>
+      </center>
+    </>
+  );
+}
+export default FoodList;
